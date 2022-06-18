aa888ac7d6bdcfc8b2d7619a7001b5b16bf03bad
abrdej <abrdej@gmail.com>
2022-06-18 10:37:43 +0200

    Update tester

--- src/tester.cpp
+++ src/tester.cpp
@@ -3,12 +3,13 @@
 #include <turn_based/managers/entity_manager.h>
 
 int main(int argc, char** argv) {
-  entities_factory factory;
-  auto shooter_id = factory.create("shooter");
-  auto saberhand_id = factory.create("saberhand");
-  auto saurian_id = factory.create("saurian");
-  auto saurian_web_id = factory.create("saurian_web");
-  auto native_id = factory.create("native");
+  game::get<entities_factory>();
+
+  auto shooter_id = game::get<entities_factory>().create("shooter");
+  auto saberhand_id = game::get<entities_factory>().create("saberhand");
+  auto saurian_id = game::get<entities_factory>().create("saurian");
+  auto saurian_web_id = game::get<entities_factory>().create("saurian_web");
+  auto native_id = game::get<entities_factory>().create("native");
 
   (void)shooter_id;
   (void)saberhand_id;
@@ -16,7 +17,7 @@ int main(int argc, char** argv) {
   (void)saurian_web_id;
   (void)native_id;
 
-  game::get<entity_manager>().clear();
+//  game::get<entity_manager>().clear();
 
   return 0;
 }
