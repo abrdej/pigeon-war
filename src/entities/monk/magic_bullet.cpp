dbb2336c3e64e5b08a1e68203abe0464c49a8e4c
abrdej <abrdej@gmail.com>
2022-07-07 21:54:00 +0200

    Adding monk

--- src/entities/monk/magic_bullet.cpp
+++ src/entities/monk/magic_bullet.cpp
@@ -1,12 +1,11 @@
 #include <magic_bullet.h>
 
+#include <turn_based/components/damage_taker.h>
+#include <turn_based/components/power_field.h>
 #include <turn_based/damage_dealers.h>
-#include <components/damage_taker.h>
-#include <components/power_field.h>
 
 magic_bullet::magic_bullet(std::uint32_t entity_id)
     : path_target_ability(3), entity_id(entity_id) {
-
   auto power = game::get<entity_manager>().get(entity_id).get<power_filed>();
 
   after_player_turn(entity_id, [this, entity_id, power]() {
@@ -28,8 +27,7 @@ magic_bullet::magic_bullet(std::uint32_t entity_id)
     }
   });
 
-  set_damage_receiver(entity_id, [this, entity_id, power](health_field& health_pack, const damage_pack& dmg) mutable {
-
+  set_damage_receiver(entity_id, [entity_id, power](health_field& health_pack, const damage_pack& dmg) mutable {
     auto half_damage = static_cast<std::int32_t>(dmg.damage_value * 0.5f);
 
     auto power_burn = std::min<std::int32_t>(power->power, half_damage);
@@ -48,21 +46,22 @@ std::string magic_bullet::hint() const {
   auto power = game::get<entity_manager>().get(entity_id).get<power_filed>();
 
   std::string desc;
-  desc = "Magic Bullet - monk accumulates " + std::to_string(magic_power_accumulation_amount) +
-         " magic points for each turn.\n"
-             "In addition, he accumulates " + std::to_string(magic_power_drain_amount) +
-         " magic points for each enemy in his neighborhood.\n"
-             "Magic point can be used to deal damage equal to the amount of magic points.\n"
-             "Additionally, half of the damage that monk receive firstly must destroys\n"
-             " the magic shiled which is formed from this points.\n"
-             "Magic power: " + std::to_string(power->power) + ".";
+  desc = "Magic Bullet - monk accumulates " + std::to_string(magic_power_accumulation_amount)
+      + " magic points for each turn.\n"
+        "In addition, he accumulates " + std::to_string(magic_power_drain_amount)
+      + " magic points for each enemy in his neighborhood.\n"
+        "Magic point can be used to deal damage equal to the amount of magic points.\n"
+        "Additionally, half of the damage that monk receive firstly must destroys\n"
+        " the magic shield which is formed from this points.\n"
+        "Magic power: " + std::to_string(power->power) + ".";
 
   return std::move(desc);
 }
 
 void magic_bullet::use(std::uint32_t index_on) {
-  if (used)
+  if (used) {
     return;
+  }
 
   auto from_index = game_control().selected_index_;
 
@@ -70,8 +69,9 @@ void magic_bullet::use(std::uint32_t index_on) {
 
   sender::send(make_action_message("magic_bullet", entity_id, index_on));
 
-  damage_dealers::standard_damage_dealer(
-      magic_damage(power->power, game_board().at(index_on), game_board().at(from_index)));
+  damage_dealers::standard_damage_dealer(magic_damage(power->power,
+                                                      game_board().at(index_on),
+                                                      game_board().at(from_index)));
 
   auto change_power = -power->power;
   power->power = 0;
---
+++ src/entities/monk/monk.cpp
@@ -0,0 +1,22 @@
+#include <magic_bullet.h>
+#include <turn_based/abilities.h>
+#include <turn_based/components/applied_effects.h>
+#include <turn_based/components/damage_taker.h>
+#include <turn_based/components/power_field.h>
+#include <turn_based/entity_factory_macro.h>
+#include <turn_based/moveable.h>
+
+DEFINE_ENTITY_FACTORY(monk) {
+  entity.name = "Monk";
+
+  entity.add<health_field>(45);
+  entity.add<power_filed>(entity.entity_id, 10, 0);
+  entity.add<applied_effects>();
+  entity.add<modification>();
+
+  auto abilities_ptr = entity.add<abilities>();
+  abilities_ptr->add_ability(std::make_shared<moveable>(3));
+  abilities_ptr->add_ability(std::make_shared<magic_bullet>(entity.entity_id));
+}
+
+FACTORY_PLUGIN_ALIAS(monk)
