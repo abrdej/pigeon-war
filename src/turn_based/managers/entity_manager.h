f5763e35b1117012b313046972ff6f7c57bac03d
abrdej <abrdej@gmail.com>
2022-08-20 09:50:17 +0200

    Minor entity_manager clean up

--- src/turn_based/managers/entity_manager.h
+++ src/turn_based/managers/entity_manager.h
@@ -1,76 +1,74 @@
 #pragma once
 
 #include <functional>
-#include <string>
 #include <unordered_map>
 #include <vector>
-
-#include <boost/dll/shared_library.hpp>
-#include <boost/dll/runtime_symbol_info.hpp>
-#include <boost/function.hpp>
 
 #include <turn_based/entity.h>
 #include <turn_based/defs.h>
 #include <turn_based/messages/messages_makers.h>
 #include <turn_based/sender.h>
 
+/**
+ * @brief Responsible to manage entities.
+ */
 class entity_manager final {
-  std::unordered_map<entity_id_t, base_entity> entities;
-  std::unordered_map<entity_id_t, std::vector<std::function<void()>>> on_destroy_callbacks;
+ public:
+  entity_id_t create(const std::function<void(base_entity& entity)>& creator) {
+    auto entity_id = generate_id();
+    auto& entity = entities_.emplace(entity_id, base_entity()).first->second;
+    entity.entity_id = entity_id;
+    creator(entity);
+    return entity_id;
+  }
 
+  base_entity get(entity_id_t entity_id) {
+    return entities_.at(entity_id);
+  }
+
+  bool alive(entity_id_t entity_id) {
+    return entities_.find(entity_id) != std::end(entities_);
+  }
+
+  void destroy(entity_id_t entity_id) {
+    entities_.erase(entity_id);
+    sender::send(make_remove_entity_message(entity_id));
+    call_destroy_callbacks(entity_id);
+  }
+
+  void on_destroy(entity_id_t entity_id, std::function<void()> callback) {
+    on_destroy_callbacks_[entity_id].push_back(std::move(callback));
+  }
+
+  void call_destroy_callbacks(entity_id_t entity_id) {
+    auto it = on_destroy_callbacks_.find(entity_id);
+    if (it != std::end(on_destroy_callbacks_)) {
+      for (const auto& callback : it->second) {
+        callback();
+      }
+      on_destroy_callbacks_.erase(it);
+    }
+  }
+
+  void clear() {
+    entities_.clear();
+    on_destroy_callbacks_.clear();
+  }
+
+  template <typename Function>
+  void for_all(Function function) {
+    for (const auto& entity_pack : entities_) {
+      function(entity_pack.second);
+    }
+  }
+
+ private:
   static entity_id_t generate_id() {
     static entity_id_t entity_id_generator{0};
     entity_id_generator = entity_id_t{static_cast<entity_id_t::value_type>(entity_id_generator) + 1};
     return entity_id_generator;
   }
 
- public:
-  inline entity_id_t create(std::function<void(base_entity& entity)> creator) {
-    auto entity_id = generate_id();
-    auto& entity = entities.emplace(entity_id, base_entity()).first->second;
-    entity.entity_id = entity_id;
-    creator(entity);
-    return entity_id;
-  }
-
-  void clear() {
-    entities.clear();
-    on_destroy_callbacks.clear();
-  }
-
-  base_entity get(entity_id_t entity_id) {
-    return entities.at(entity_id);
-  }
-
-  inline bool alive(entity_id_t entity_id) {
-    return entities.find(entity_id) != std::end(entities);
-  }
-
-  inline void destroy(entity_id_t entity_id) {
-    entities.erase(entity_id);
-
-    sender::send(make_remove_entity_message(entity_id));
-
-    call_destroy_callbacks(entity_id);
-  }
-
-  void on_destroy(entity_id_t entity_id, std::function<void()> callback) {
-    on_destroy_callbacks[entity_id].push_back(std::move(callback));
-  }
-
-  void call_destroy_callbacks(entity_id_t entity_id) {
-    auto it = on_destroy_callbacks.find(entity_id);
-    if (it != std::end(on_destroy_callbacks)) {
-      for (auto&& callback : it->second)
-        callback();
-      on_destroy_callbacks.erase(it);
-    }
-  }
-
-  template <typename Function>
-  void for_all(Function function) {
-    for (auto&& entity_pack : entities) {
-      function(entity_pack.second);
-    }
-  }
+  std::unordered_map<entity_id_t, base_entity> entities_;
+  std::unordered_map<entity_id_t, std::vector<std::function<void()>>> on_destroy_callbacks_;
 };
